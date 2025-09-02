d37eca6de8c33e6ea14a82624887e1bb736b5dcc
D4rK14 <mastahoscuro@gmail.com>
2025-09-01 22:19:51 -0400

    cambios main

--- app/src/main/java/com/example/eccomerce/MainActivity.java
+++ app/src/main/java/com/example/eccomerce/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.eccomerce;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        ImageButton btnVerRecetasEmpanadas = findViewById(R.id.recetasEmpanadas);
+
+        btnVerRecetasEmpanadas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, actividad5.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
