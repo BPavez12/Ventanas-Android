036a79baf2f1c59d99b47f1623da0b962f44f591
BPavez12 <b.pavez12@alumnos.santotomas.cl>
2025-09-11 13:16:33 -0300

    Se realizo merge desde master a testbranch y se cambio el color de fondo de la burbuja de texto y se agrego la activity parrillasTodos a manifest
    Se realizo merge desde master a testbranch y se cambio el color de fondo de la burbuja de texto y se agrego la activity parrillasTodos a manifest

--- app/src/main/java/com/example/eccomerce/ParrillasTodas.java
+++ app/src/main/java/com/example/eccomerce/ParrillasTodas.java
@@ -13,14 +13,10 @@ public class ParrillasTodas extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Habilita Edge-to-Edge para que el layout ocupe toda la pantalla
         EdgeToEdge.enable(this);
-
         // Asociar el layout
         setContentView(R.layout.activity_parrillas_todas);
-
-        // Ajusta el padding de la view raíz para que no se solape con las barras de sistema
+        // Ajusta el padding de la vista de la raíz para que no se choque con las barras del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainParrillas), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
